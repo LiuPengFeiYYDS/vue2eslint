0e1523e702a689b146a74bfdc2c7fb906df56eda
lpf <2792717765@qq.com>
2022-07-11 17:39:00 +0800

    feat(修改): 修改

--- vue.config.js
+++ vue.config.js
@@ -1,17 +1,16 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath:'./',
-  devServer:{
-    port:8080,
-    host:'localhost',
-    https:false,
-    proxy:{
-      [process.env.VUE_APP_BASE_API]:{
-        target:VUE_APP_BASE_URL,
-        changeOrigin:true,
-        pathWrite:{
-          ['^'+process.env.VUE_APP_BASE_API]:''
+  devServer: {
+    open: false,
+    port: 8081,
+    https: false,
+    proxy: {
+      [process.env.VUE_APP_BASE_API]: {
+        target: process.env.VUE_APP_BASE_URL,
+        changeOrigin: true,
+        pathRewrite: {
+          ['^' + process.env.VUE_APP_BASE_API]: ''
         }
       }
     }
