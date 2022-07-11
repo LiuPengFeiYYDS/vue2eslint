f2c2682aa9715056e689600ad807958afa0cd5a4
supperT <97022976@QQ.com>
2022-07-11 16:30:20 +0800

    feat: 设置跨域，添加请求拦截器

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,19 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  publicPath:'./',
+  devServer:{
+    port:8080,
+    host:'localhost',
+    https:false,
+    proxy:{
+      [process.env.VUE_APP_BASE_API]:{
+        target:VUE_APP_BASE_URL,
+        changeOrigin:true,
+        pathWrite:{
+          ['^'+process.env.VUE_APP_BASE_API]:''
+        }
+      }
+    }
+  }
 })
